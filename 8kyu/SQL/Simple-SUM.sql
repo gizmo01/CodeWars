185353f869b30f3fd7adf57acd4b90bf57bb08c9
Mario A <quaaludes@gmail.com>
2019-04-17 18:01:51 +0200

    Create Simple-SUM.sql

---
+++ 8kyu/SQL/Simple-SUM.sql
@@ -0,0 +1,14 @@
+/*
+For this challenge you need to create a simple SUM statement that will sum all the ages.
+people table schema
+
+    id
+    name
+    age
+
+select table schema
+
+    age_sum (sum of ages)
+*/
+
+SELECT sum(age) as age_sum  from people
