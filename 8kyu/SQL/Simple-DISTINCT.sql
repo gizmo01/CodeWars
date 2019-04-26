cc06fe71ec29966fee97f7675477f6bcfb32947e
Mario A <quaaludes@gmail.com>
2019-04-26 14:37:46 +0200

    Create Simple-DISTINCT.sql

---
+++ 8kyu/SQL/Simple-DISTINCT.sql
@@ -0,0 +1,14 @@
+/*
+For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+people table schema
+
+    id
+    name
+    age
+
+select table schema
+
+    age (distinct)
+*/
+
+select DISTINCT(age) from people
