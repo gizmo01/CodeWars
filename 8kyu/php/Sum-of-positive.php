91b49a145d478ee811b13c51e45682e9158985ee
Mario A <quaaludes@gmail.com>
2019-05-08 11:33:47 +0200

    Create Sum-of-positive.php

---
+++ 8kyu/php/Sum-of-positive.php
@@ -0,0 +1,17 @@
+<?
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+function positive_sum($arr) {
+  $sum = 0;
+  foreach ($arr as $item){
+  if($item>0){$sum +=$item;}
+  }
+  return $sum;
+}
+>
