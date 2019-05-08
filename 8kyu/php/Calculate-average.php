b930cea08f5791fffc150e40f4a074d1ce098747
Mario A <quaaludes@gmail.com>
2019-05-08 12:04:48 +0200

    Create Calculate-average.php

---
+++ 8kyu/php/Calculate-average.php
@@ -0,0 +1,10 @@
+<?
+/*
+Write function avg which calculates average of numbers in given list.
+*/
+
+function find_average($array) {
+  // Write your code here
+  return array_sum($array)/count($array);
+}
+>
